7cf8cdd60a043b87efe421cc61dbced86e0996bf
annnik <nikishina.a.l@gmail.com>
2014-04-23 08:40:18 +0400

    Uploading test program

---
+++ testprogram/src/com/assignment0/testprogram/HelloWorldActivity.java
@@ -0,0 +1,17 @@
+package com.assignment0.testprogram;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class HelloWorldActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_helloworld);
+      
+    }
+
+}
+   
